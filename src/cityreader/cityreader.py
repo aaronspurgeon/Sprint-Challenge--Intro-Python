97b56c26a253f4fd5ecff2ca75275087fe62990b
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-06-12 21:09:34 -0400

    Test failing but Im doing what is required

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,17 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
+filename = 'cities.csv'
+
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lon = lon
+        self.lat = lat
+
+    # def __str__(self):
+    #     return f'{self.name}, {self.lat}, {self.lon}'
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -15,12 +27,22 @@
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 cities = []
+fields = []
+rows = []
+
+# city,state_name,county_name,lat,lng,population,density,timezone,zips
 
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the
-  # `cities` list
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)
+
+        for row in csvreader:
+            cities.append(City(row[0], row[3], row[4]))
 
     return cities
 
@@ -29,7 +51,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+    print(c.name, c.lat, c.lon)
 
 # STRETCH GOAL!
 #
@@ -64,7 +86,7 @@ for c in cities:
 
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
+    # within will hold the cities that fall within the specified region
     within = []
 
     # TODO Ensure that the lat and lon valuse are all floats
